9bb21ca45c3180aa5bb83b15f2c6397dd221cbbb
DELL <DELL@Qing>
2019-03-28 15:39:28 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this my first time!");
+		System.out.println("我的第一个终极项目修改完成！");
 	}
 
 }
